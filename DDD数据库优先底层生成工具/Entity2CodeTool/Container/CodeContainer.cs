17c8d955443d0ec3c7858f9c70afe7f4d7c3b976
comicwang <672802899@qq.com>
2016-06-13 18:37:57 +0800

    Base
    Base

---
+++ DDD数据库优先底层生成工具/Entity2CodeTool/Container/CodeContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infoearth.Entity2CodeTool
+{
+    public class CodeContainer
+    {
+        public const string StaticCode = "";
+    }
+}
---
+++ DDD数据库优先底层生成工具/Entity2CodeTool/UI/FormAddMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Infoearth.Entity2CodeTool.UI
+{
+    public partial class FormAddMethod : Form
+    {
+        public FormAddMethod()
+        {
+            InitializeComponent();
+        }
+    }
+}
